f85e6c7abf10a778482b11c71ec6a2d7de2c21b4
Lars Grefer <github@larsgrefer.de>
2015-08-05 19:55:02 +0200

    # cleanup code and use lambda expressions

--- src/main/java/sample/MainController.java
+++ src/main/java/sample/MainController.java
@@ -3,23 +3,18 @@ package sample;
 import com.sun.glass.ui.Size;
 import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.text.*;
 import sample.ImageGenerating.*;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.Font;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.awt.image.Raster;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
